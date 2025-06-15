bffdae8a105fb3d3cb89954902ea76ce5109b0a2
faelsilvacruz <scruz.raphael@gmail.com>
2025-06-15 16:42:57 -0400

    Update init_db.py

--- init_db.py
+++ init_db.py
@@ -1,19 +1,31 @@
 import sqlite3
+from login_page import make_hashes
 
 def init_db():
     # Criar users.db
     conn_users = sqlite3.connect('users.db')
     c_users = conn_users.cursor()
     c_users.execute('CREATE TABLE IF NOT EXISTS userstable(username TEXT, password TEXT, role TEXT)')
-    c_users.execute('INSERT INTO userstable VALUES ("admin", "e99a18c428cb38d5f260853678922e03", "admin")')  # senha: abc123
+
+    # Inserir usuário admin com senha hash de 'abc123'
+    hashed_password = make_hashes("abc123")
+    c_users.execute('INSERT INTO userstable VALUES (?, ?, ?)', ("admin", hashed_password, "admin"))
+
     conn_users.commit()
     conn_users.close()
 
     # Criar holerites.db
     conn_holerite = sqlite3.connect('holerites.db')
     c_holerite = conn_holerite.cursor()
-    c_holerite.execute('CREATE TABLE IF NOT EXISTS holerites (id INTEGER PRIMARY KEY AUTOINCREMENT, nome_colaborador TEXT, mes TEXT, ano TEXT, link_google_drive TEXT)')
-    c_holerite.execute('INSERT INTO holerites (nome_colaborador, mes, ano, link_google_drive) VALUES ("admin", "Maio", "2025", "https://google.com")')
+    c_holerite.execute('''
+        CREATE TABLE IF NOT EXISTS holerites (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nome_colaborador TEXT,
+            mes TEXT,
+            ano TEXT,
+            link_google_drive TEXT
+        )
+    ''')
     conn_holerite.commit()
     conn_holerite.close()
 

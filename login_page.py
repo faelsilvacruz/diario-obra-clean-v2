a515853ae55a23c0cc57ab6fd91f52a1ce782a7f
faelsilvacruz <scruz.raphael@gmail.com>
2025-06-15 15:47:03 -0400

    Update login_page.py

--- login_page.py
+++ login_page.py
@@ -2,14 +2,12 @@ import streamlit as st
 import sqlite3
 import hashlib
 
-# Funções de hash
 def make_hashes(password):
     return hashlib.sha256(str.encode(password)).hexdigest()
 
 def check_hashes(password, hashed_text):
     return make_hashes(password) == hashed_text
 
-# Banco de dados
 conn = sqlite3.connect('users.db')
 c = conn.cursor()
 
@@ -18,43 +16,42 @@ def login_user(username, password):
     data = c.fetchall()
     return data
 
-# CSS customizado
-st.markdown(
-    """
-    <style>
-    .stApp {
-        background-color: #0F2A4D;
-    }
-    .titulo-principal {
-        text-align: center;
-        color: white;
-        font-size: 30px;
-        margin-top: 40px;
-        margin-bottom: 30px;
-        font-weight: bold;
-    }
-    .login-inputs {
-        max-width: 400px;
-        margin: auto;
-        background: transparent;
-    }
-    .logo-rodape {
-        margin-top: 50px;
-        text-align: center;
-    }
-    .logo-rodape img {
-        display: block;
-        margin-left: auto;
-        margin-right: auto;
-        max-width: 250px;
-        height: auto;
-        width: 100%;
-    }
-    </style>
-    """, unsafe_allow_html=True
-)
+def render_login_page():
+    st.markdown(
+        """
+        <style>
+        .stApp {
+            background-color: #0F2A4D;
+        }
+        .titulo-principal {
+            text-align: center;
+            color: white;
+            font-size: 30px;
+            margin-top: 40px;
+            margin-bottom: 30px;
+            font-weight: bold;
+        }
+        .login-inputs {
+            max-width: 400px;
+            margin: auto;
+            background: transparent;
+        }
+        .logo-rodape {
+            margin-top: 50px;
+            text-align: center;
+        }
+        .logo-rodape img {
+            display: block;
+            margin-left: auto;
+            margin-right: auto;
+            max-width: 250px;
+            height: auto;
+            width: 100%;
+        }
+        </style>
+        """, unsafe_allow_html=True
+    )
 
-def main():
     st.markdown('<div class="titulo-principal">Acesso ao Diário de Obra</div>', unsafe_allow_html=True)
 
     with st.container():
@@ -68,7 +65,9 @@ def main():
             result = login_user(username, hashed_pswd)
             if result:
                 st.success(f'Bem-vindo, {username}! Login realizado com sucesso.')
-                st.info('Aqui você carregaria o restante do app...')
+                st.session_state["username"] = username
+                st.session_state["logged_in"] = True
+                st.rerun()
             else:
                 st.error('Usuário ou senha inválidos.')
 
@@ -77,6 +76,3 @@ def main():
     st.markdown('<div class="logo-rodape">', unsafe_allow_html=True)
     st.image('logo_rdv.png', use_container_width=True)
     st.markdown('</div>', unsafe_allow_html=True)
-
-if __name__ == '__main__':
-    main()

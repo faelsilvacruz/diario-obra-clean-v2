416e15862834f9f385bb4a37d5fbd525b04cce5a
faelsilvacruz <scruz.raphael@gmail.com>
2025-06-15 16:40:47 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,13 +3,8 @@ from login_page import render_login_page
 from user_management_page import render_user_management_page
 from diario_obra_page import render_diario_obra_page
 from holerite_page import render_holerite_page
-from init_db import init_db
 
 st.set_page_config(page_title="Sistema RDV", layout="centered")
-
-if st.sidebar.button("Rodar INIT_DB"):
-    init_db()
-    st.success("Bancos criados com sucesso!")
 
 if 'logged_in' not in st.session_state:
     st.session_state.logged_in = False

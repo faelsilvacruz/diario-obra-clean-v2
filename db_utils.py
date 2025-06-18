1235ec46008ba5f022d2ca4306b7bc3afe764222
faelsilvacruz <scruz.raphael@gmail.com>
2025-06-18 15:07:51 -0400

    Update db_utils.py

--- db_utils.py
+++ db_utils.py
@@ -7,3 +7,24 @@ def get_obras():
     obras = c.fetchall()
     conn.close()
     return obras
+
+def add_obra(nome_obra):
+    conn = sqlite3.connect('diario_obra.db')
+    c = conn.cursor()
+    c.execute('INSERT INTO obras (nome) VALUES (?)', (nome_obra,))
+    conn.commit()
+    conn.close()
+
+def add_contrato(obra_id, nome_contrato):
+    conn = sqlite3.connect('diario_obra.db')
+    c = conn.cursor()
+    c.execute('INSERT INTO contratos (obra_id, nome) VALUES (?, ?)', (obra_id, nome_contrato))
+    conn.commit()
+    conn.close()
+
+def add_colaborador(nome_colaborador, funcao):
+    conn = sqlite3.connect('diario_obra.db')
+    c = conn.cursor()
+    c.execute('INSERT INTO colaboradores (nome, funcao) VALUES (?, ?)', (nome_colaborador, funcao))
+    conn.commit()
+    conn.close()
